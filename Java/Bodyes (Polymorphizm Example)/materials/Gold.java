60af2534fb9063a45c3a8fe3fb6db87a540aa58d
artemjackson <artemjackson95@gmail.com>
2014-03-30 22:17:45 +0300

    We want bonus!

---
+++ Java/Bodyes (Polymorphizm Example)/materials/Gold.java
@@ -0,0 +1,41 @@
+package materials;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Gold extends Material{
+	public Gold(){
+		property.add("briliant");
+		property.add("expensive");
+		property.add("heavy");	
+	}
+	
+	@Override
+	public String[] getProperties()
+    	{
+		ArrayList<String> temp = new ArrayList<String>();
+
+		Random rand = new Random();
+       		
+		int i = rand.nextInt(10);				
+		if(i > 3)
+			temp.add(property.get(0));
+
+		i = rand.nextInt(10);
+		if(i > 8)		
+			temp.add(property.get(1));
+		
+		i = rand.nextInt(10);
+		if(i > 5)		
+			temp.add(property.get(1));
+
+		if(temp.size() == 0){
+			i = rand.nextInt(3);
+			temp.add(property.get(i));
+		}
+
+		String[] output = new String[temp.size()];
+		temp.toArray(output);
+		return output;
+	}
+}
---
+++ Java/Bodyes (Polymorphizm Example)/shapes/Tetraider .java
@@ -0,0 +1,40 @@
+package shapes;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class Tetraider extends Shape3D
+{
+	public Tetraider()
+	{
+		property.add("angular");
+		property.add("empty");
+	}
+	
+	@Override
+	public String[] getProperties()
+    	{
+		ArrayList<String> temp = new ArrayList<String>();
+
+		Random rand = new Random();
+       		
+		int i = rand.nextInt(10);				
+		if(i > 3)
+			temp.add(property.get(0));
+
+		i = rand.nextInt(10);
+		if(i > 7)		
+			temp.add(property.get(1));
+		
+		if(temp.size() == 0){
+			i = rand.nextInt(2);
+			temp.add(property.get(i));
+		}
+		
+		
+		String[] output = new String[temp.size()];
+		temp.toArray(output);
+		return output;
+	}
+}
+
