021a8e8c520817376726759d8fbb943864419740
artemjackson <artemjackson95@gmail.com>
2014-04-06 11:20:24 +0300

    D lang folder is added

---
+++ D/Sleep Sort/hello.d
@@ -0,0 +1,34 @@
+module main;
+
+import std.stdio;
+import core.thread;
+import std.conv;
+import std.concurrency;
+
+/**
+*   This program was wrotted just for fun
+*   and for studying spawn function in D language
+*   This 'algorithm' works for O(m), where m is max inputed value
+*/
+
+void f(int i)
+{
+    Thread.sleep(dur!("msecs")(i));
+    //  For too slow computers use:
+    //  Thread.sleep(dur!("seconds")(i));
+    writeln(i);
+}
+
+int main(string[] args)
+in
+{
+    assert(args.length > 1, "Please, enter some integers to sortn.\n");
+}
+body
+{
+    if(args.length > 2)
+        foreach(i; 1..args.length)
+            spawn(&f, to!int(args[i]));
+
+    return 0;
+}
